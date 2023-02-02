24f4eb27ce9bdc764ae0c3020ae59954767d97f8
garaisss <renars.ikaunieks@cube.lv>
2023-02-02 16:22:19 +0200

    initial commit

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,27 @@
+<!doctype html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+    <article>
+        <h1><a href="/posts/my-first-post">First Text</a></h1>
+
+        <p>Their could can widen ten she any. As so we smart those money in.</p>
+
+    </article>
+
+    <article>
+        <h1><a href="/posts/my-second-post">Second Text</a></h1>
+
+        <p>Preserved defective offending he daughters on or.</p>
+
+    </article>
+
+    <article>
+        <h1><a href="/posts/my-third-post">Third Text</a></h1>
+
+        <p>No it as breakfast up conveying earnestly immediate principle.</p>
+
+    </article>
+</body>
