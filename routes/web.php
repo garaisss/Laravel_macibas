1a118d2feb289dc97972d5287336d1b7bc081c94
garaisss <renars.ikaunieks@cube.lv>
2023-02-12 01:25:41 +0200

    kaut kas no JS

--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,8 @@ use League\CommonMark\Extension\FrontMatter\Data\SymfonyYamlFrontMatterParser;
 
 Route::get('/', function () {
     return view('posts', [
-        'posts' => Post::latest()->get()
+        'posts' => Post::latest()->get(),
+        'categories' => Category::all()
     ]);
 });
 
@@ -39,12 +40,15 @@ Route::get("posts/{post:slug}", function (Post $post) {
 
 Route::get('categories/{category:slug}', function(Category $category) {
     return view('posts', [
-        'posts' => $category->posts
+        'posts' => $category->posts,
+        'currentCategory' => $category,
+        'categories' => Category::all()
     ]);
 });
 
 Route::get('authors/{author:username}', function(User $author) {
     return view('posts', [
-        'posts' => $author->posts
+        'posts' => $author->posts,
+        'categories' => Category::all()
     ]);
 });
