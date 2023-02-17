4782ccbd4d9f0967cb3b2dfee5e1d6f217608c65
garaisss <renars.ikaunieks@cube.lv>
2023-02-17 17:23:23 +0200

    nelielas izmaiņas (RegisterController izveidošana)

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -14,7 +14,7 @@ class PostController extends Controller
     return view('posts.index', [
         'posts' => Post::latest()->filter(
             request(['search', 'category', 'author']))
-            ->paginate(3),
+            ->paginate(3)->withQueryString()
     ]);
     }
 
--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,12 @@
 use App\Models\Post;
 use App\Models\User;
 use App\Models\Category;
-use App\Http\Controllers\PostController;
-
 use Illuminate\Support\Facades\Route;
+
 use Facade\FlareClient\Stacktrace\File;
+use App\Http\Controllers\PostController;
 use Spatie\YamlFrontMatter\YamlFrontMatter;
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\File as FacadesFile;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use League\CommonMark\Extension\FrontMatter\Data\LibYamlFrontMatterParser;
@@ -19,3 +20,5 @@ Route::get('/', [PostController::class, 'index'])->name('home');
 Route::get("posts/{post:slug}", [PostController::class, 'show']);
 
 Route::get("category/{category:slug}", [PostController::class, 'show']);
+
+Route::get('register', [RegisterController::class, 'create']);
