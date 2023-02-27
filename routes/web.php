c7fd2aad5f4bbb25c6733f6a6ef1222070aceb6f
garaisss <renars.ikaunieks@cube.lv>
2023-02-27 14:31:28 +0200

    jauna posta izveidosana

--- routes/web.php
+++ routes/web.php
@@ -1,31 +1,19 @@
 <?php
 
-use MailchimpMarketing\ApiClient;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionsController;
+use App\Http\Controllers\NewsletterController;
 use App\Http\Controllers\PostCommentsController;
-
-
-Route::get('ping', function () {
-    $mailchimp = new ApiClient();
-
-    $mailchimp->setConfig([
-        'apiKey' => config('services.mailchimp.key'),
-        'server' => 'us11'
-    ]);
-
-    $response = $mailchimp->lists->getList('4a4b449090');
-    // $response = $mailchimp->lists->getAllLists();
-    ddd($response);
-});
 
 
 Route::get('/', [PostController::class, 'index'])->name('home');
 
 Route::get("posts/{post:slug}", [PostController::class, 'show']);
 Route::post("posts/{post:slug}/comments", [PostCommentsController::class, 'store']);
+
+Route::get('newsletter', NewsletterController::class);
 
 Route::get('register', [RegisterController::class, 'create'])->middleware('guest');
 Route::post('register', [RegisterController::class, 'store'])->middleware('guest');
@@ -34,3 +22,6 @@ Route::get('login', [SessionsController::class, 'create'])->middleware('guest');
 Route::post('login', [SessionsController::class, 'store'])->middleware('guest');
 
 Route::post('logout', [SessionsController::class, 'destroy'])->middleware('auth');
+
+Route::get('admin/posts/create', [PostController::class, 'create'])->middleware('admin');
+Route::post('admin/posts', [PostController::class, 'store'])->middleware('admin');
